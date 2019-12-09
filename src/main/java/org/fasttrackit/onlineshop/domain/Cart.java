40f78ba35038dd513b6e6941a38e9a78e0debdf4
Catalin <catalin.radau@yahoo.com>
2019-12-09 21:20:19 +0200

    Added Many to Many relationship cart-product (Work in progress)

--- src/main/java/org/fasttrackit/onlineshop/domain/Cart.java
+++ src/main/java/org/fasttrackit/onlineshop/domain/Cart.java
@@ -1,6 +1,8 @@
 package org.fasttrackit.onlineshop.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Cart {
@@ -9,6 +11,11 @@ public class Cart {
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
     private Customer customer;
+
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "cart_product", joinColumns = @JoinColumn(name = "cart_id"),
+    inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private Set<Product> products = new HashSet<>();
 
     public Long getId() {
         return id;
@@ -26,11 +33,34 @@ public class Cart {
         this.customer = customer;
     }
 
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
     @Override
     public String toString() {
         return "Cart{" +
                 "id=" + id +
                 ", customer=" + customer +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cart cart = (Cart) o;
+
+        return id.equals(cart.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
     }
 }
